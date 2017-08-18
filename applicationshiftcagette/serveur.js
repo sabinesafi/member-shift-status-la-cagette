44f8d96204f02f69117cf2fe6e01860c0309012a
Travis CI <stephane.langlois@scopyleft.fr>
2017-08-18 20:09:38 +0200

    Fix

--- applicationshiftcagette/serveur.js
+++ applicationshiftcagette/serveur.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const http = require('http')
 const app = express()
+
 
 app.get('/', (req, res) =>
     res.send('Hello World!')
 )
 
 app.listen(3000, () =>
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!')
 )
